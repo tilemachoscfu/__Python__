f6e4fc5c223cf6acffecf5acc58eced05ac3fb6f
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-10-26 13:48:52 +0300

    Add files via upload

---
+++ convertCelsius_Fahr/convertCelsius_Fahr.py
@@ -0,0 +1,9 @@
+# Python Program to convert temperature in celsius to fahrenheit
+
+# User input
+celsius = float(input('Give a temperature(Celsius): '))
+
+# Calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+print ('%s degree Celsius is equal to %s degree Fahrenheit' %(celsius,fahrenheit))
